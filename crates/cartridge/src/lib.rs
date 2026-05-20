d961098eb741e26460c75004ab7f0e8c367be4e4
ajilenakh <107887918+ajilenakh@users.noreply.github.com>
2026-05-21 04:49:30 +0530

    feature: load critridge from rom add cartridge headers (title and type)

--- crates/cartridge/src/lib.rs
+++ crates/cartridge/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fs;
+use std::io;
+
+// RAW Cartridge DATA
+pub struct Cartridge {
+    rom: Vec<u8>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, PartialEq)]
+pub enum CartridgeType {
+    RomOnly,
+    MBC1,
+    MBC1Ram,
+    MBC1RamBattery,
+    Unknown(u8),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Cartridge {
+    // Load cartridge from rom
+    pub fn load(path: &str) -> io::Result<Self> {
+        let rom = fs::read(path)?;
+        Ok(Self { rom })
+    }
+
+    // Extrat rom title
+    pub fn title(&self) -> String {
+        let title_bytes = &self.rom[0x0134..0x0144];
+        // Convert Bytes to ASCII
+        let title = String::from_utf8_lossy(title_bytes).trim_end_matches('\0').to_string();
+        title
+    }
+
+    // Extract cartridge type
+    pub fn cartridge_type(&self) -> CartridgeType {
+        match self.rom[0x0147] {
+            0x00 => CartridgeType::RomOnly,
+            0x01 => CartridgeType::MBC1,
+            0x02 => CartridgeType::MBC1Ram,
+            0x03 => CartridgeType::MBC1RamBattery,
+            other => CartridgeType::Unknown(other),
+        }
     }
 }
